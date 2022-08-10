d27a483c783a0cfd83a13a2d0b59891b55b1703f
HappyAreaBean <bean@happyareabean.cc>
2022-08-10 15:27:28 +0800

    refactor(config): Updated docusaurus.config.js url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Imanity Docs',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.imanity.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
